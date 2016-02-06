00ddd33fdeceaaca9e82ce1e684abc3b38af9bb7
nabil-boag <nabilgod@gmail.com>
2016-02-06 16:31:15 +0000

    Add db test

--- php-app/index.php
+++ php-app/index.php
@@ -1,3 +1,11 @@
 <h1>Hello World!!!</h1>
-<h3>PHP Version <?= phpversion() ?></h3>
-<a href="/static.html">Static HTML Page</a>
+<h3><?= phpinfo() ?></h3>
+
+<?php
+# Fill our vars and run on cli
+# $ php -f db-connect-test.php
+$dbuser = 'elasticuser';
+$dbpass = 'pass';
+$dbhost = 'host';
+$connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+mysql_select_db($dbname) or die("Could not open the db '$dbname'");
